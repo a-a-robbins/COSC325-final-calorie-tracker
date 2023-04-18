2c27e2adb52d39106b485855834b3e1ef9008a91
a-a-robbins <robbinsa1@central.edu>
2023-04-18 10:51:57 -0500

    add background

--- CalorieTracker/CalorieTracker/CalorieTracker/ContentView.swift
+++ CalorieTracker/CalorieTracker/CalorieTracker/ContentView.swift
@@ -12,20 +12,40 @@ struct ContentView: View {
     @EnvironmentObject var todayCalories: DayCalories
     
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
-            AddView()
-                .tabItem {
-                    Label("Add Food", systemImage: "plus.circle")
-                }
-            DayView()
-                .tabItem {
-                    Label("Today's Diary", systemImage: "eye")
-                }
+        ZStack {
+            GradientBackground()
+            
+            //setting tabView background
+            //courtesy: https://www.bigmountainstudio.com/community/public/posts/86559-how-do-i-customize-the-tabview-in-swiftui
+            
+            VStack {
+                Spacer()
+                
+                Color.blue
+                    .frame(height: 100)
+            }
+            .ignoresSafeArea(edges: .bottom)
+            //end Big Mountain Studio code
+            
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                AddView()
+                    .tabItem {
+                        Label("Add Food", systemImage: "plus.circle")
+                    }
+                DayView()
+                    .tabItem {
+                        Label("Today's Diary", systemImage: "eye")
+                    }
+            }
+            .toolbarBackground(.blue, for: .tabBar)
         }
+        .opacity(0.75)
+
+            
         Spacer()
         .padding()
     }
