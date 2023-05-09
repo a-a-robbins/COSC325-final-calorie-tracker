3dc0233a9c072e7bb13b1cb6de4142f26ac09974
a-a-robbins <a.a.robbins@outlook.com>
2023-05-09 08:01:24 -0500

    Add files via upload

---
+++ CalorieTracker/CalorieTracker/CalorieTracker/Helpers/FavoriteButton.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteButton.swift
+//  CalorieTracker
+//
+//  Created by ARobbins on 5/8/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
+
